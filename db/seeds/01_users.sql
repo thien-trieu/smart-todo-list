eccf8cbe132d9cc640ce73766d41d476ed9da961
Joe Polo <joe.polo@telus.com>
2022-12-17 13:06:39 -0800

    update user seed file with test users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Britney', 'itsbritney@spears.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Beyonce', 'kirauser2@any.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Justin', 'user3@timberlake.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+INSERT INTO users (name, email, password) VALUES ('Slimshady', 'marshal@mathers.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+INSERT INTO users (name, email, password) VALUES ('Frodo', 'mrunderhill@shire.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+
