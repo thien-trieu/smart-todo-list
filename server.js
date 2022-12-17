6798d55152e7beac1550275bf7c31353b944325c
Thien Trieu <thien.trieu@telus.com>
2022-12-17 14:24:49 -0800

    add login and register ejs files in views folder.  add app.get routes for login and register in server.js file.

--- server.js
+++ server.js
@@ -48,6 +48,15 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
