395d1fcdd57c6f9b9f68a1c720df5cafbb7cea5b
Thien Trieu <thien.trieu@telus.com>
2022-12-19 10:16:45 -0800

    start router.post for register route

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
+
   res.render('register');
 });
+
+router.post('/', (req, res) => {
+  const userName = req.body.name
+  const userEmail = req.body.email
+  const userPassword = req.body.password
+
+  res.redirect('/')
+})
 
 module.exports = router;
