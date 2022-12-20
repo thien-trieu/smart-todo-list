4215fc713ea2578bcf225417d1cec2317eedcacb
Jhon Useche <jhon.useche@telus.com>
2022-12-19 19:58:24 -0700

    Fix revert

--- routes/register.js
+++ routes/register.js
@@ -20,29 +20,25 @@ router.get('/', (req, res) => {
   });
 });
 
-
 router.post('/', [
   body('name')
     .trim()
-    .notEmpty().withMessage(" Please enter your name "),
+    .notEmpty().withMessage("The email field cannot be empty!"),
   body('email')
     .trim()
     .notEmpty()
-    .withMessage(" Please enter your email ")
+    .withMessage("The email field cannot be empty!")
     .normalizeEmail()
     .toLowerCase(),
   body('password')
     .trim()
     .notEmpty()
-    .withMessage(" Please create a strong password ")
-
+    .withMessage("The password field cannot be empty!")
 ], (req, res) => {
-
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     console.log(errors);
-
     const templateVars = {
       errors: errors.array(),
       user: undefined
@@ -50,13 +46,11 @@ router.post('/', [
     // display the errors
     return res.render('register', templateVars);
   }
-
   const newUser = {
     name: req.body.name,
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 10)
   };
-
   // if registration is successful, add user info to db
   addUser(newUser)
     .then(getUserByEmail(req.body.email))
@@ -69,7 +63,6 @@ router.post('/', [
       res.redirect('/');
       return;
     });
-
 });
 
 module.exports = router;
