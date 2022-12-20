8f5df30806089d04b6573d7b7202009be974ce58
Jhon Useche <jhon.useche@telus.com>
2022-12-19 19:58:24 -0700

    Fix revert

--- routes/register.js
+++ routes/register.js
@@ -3,19 +3,42 @@ const router  = express.Router();
 const bcrypt = require('bcryptjs');
 const { body, check, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => {
+const { addUser, getUserByEmail, getUserById } = require('../db/queries/users');
 
-  res.render('register');
+router.get('/', (req, res) => {
+  const userId = req.session.userID;
+  if (userId) return res.redirect("/");
+  console.log('USER ID', req.session.userID);
+
+  getUserById(userId).then((user)=>{
+    console.log('USER', user);
+    const templateVars = {
+      user,
+      errors: null
+    };
+    res.render('register', templateVars);
+  });
 });
 
-
-router.post('/', (req, res) => {
-
+router.post('/', [
+  body('name')
+    .trim()
+    .notEmpty().withMessage("The email field cannot be empty!"),
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage("The email field cannot be empty!")
+    .normalizeEmail()
+    .toLowerCase(),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage("The password field cannot be empty!")
+], (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     console.log(errors);
-
     const templateVars = {
       errors: errors.array(),
       user: undefined
@@ -23,13 +46,11 @@ router.post('/', (req, res) => {
     // display the errors
     return res.render('register', templateVars);
   }
-
   const newUser = {
     name: req.body.name,
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 10)
   };
-
   // if registration is successful, add user info to db
   addUser(newUser)
     .then(getUserByEmail(req.body.email))
@@ -42,7 +63,6 @@ router.post('/', (req, res) => {
       res.redirect('/');
       return;
     });
-
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
 const cookieSession = require('cookie-session');
-const bcrypt = require('bcryptjs');
 const { getUserById } = require('./db/queries/users');
 
 
