d858761c9c1620de3ca0576594ad18f09e5f477e
Jhon Useche <jhon.useche@telus.com>
2022-12-19 19:01:26 -0700

    Update the todo html to change icons and add a select for the categories.

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcryptjs');
 const { body, check, validationResult } = require("express-validator");
 
 const { addUser, getUserByEmail, getUserById } = require('../db/queries/users');
@@ -36,34 +37,39 @@ router.post('/', [
     .withMessage(" Please create a strong password ")
 
 ], (req, res) => {
-  const errors = validationResult(req)
 
-  if (!errors.isEmpty()){
-    console.log(errors)
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+
     const templateVars = {
       errors: errors.array(),
       user: undefined
-    }
-      // display the errors
-      return res.render('register', templateVars)
-    }
-    const newUser = {
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10)
-    }
-    // if registration is successful, add user info to db
+    };
+    // display the errors
+    return res.render('register', templateVars);
+  }
+
+  const newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10)
+  };
+
+  // if registration is successful, add user info to db
   addUser(newUser)
-  .then(getUserByEmail(req.body.email))
-  .then((user)=> {
-    console.log('user object', user)
-    console.log('user.id', user.id)
+    .then(getUserByEmail(req.body.email))
+    .then((user)=> {
+      console.log('user object', user);
+      console.log('user.id', user.id);
 
 
-    req.session.userID = user.id
-    res.redirect('/')
-    return
-  })
-})
+      req.session.userID = user.id;
+      res.redirect('/');
+      return;
+    });
+
+});
 
 module.exports = router;
