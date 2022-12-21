50b078c80e7cfaa57fedcd279c2442f039b34f11
Thien Trieu <thien.trieu@telus.com>
2022-12-21 15:24:45 -0800

    create selectcategory function and callyelp api

--- services/apis/yelp.js
+++ services/apis/yelp.js
@@ -1,16 +1,36 @@
 const axios = require('axios');
 
-const options = {
-  headers: {
-    accept: 'application/json',
-    Authorization: 'jI8tcWkGvSze503qbpZDTTcX03EOu_WQQTM6_szO8eItEuBqCdSr-Y8Mu51KSkjqElalp0EqHcKxRA6Zs_cwUsCvK6Ww0tkr9lANZcGqvOfflxtbPPbrRe0hkUKjY3Yx'
-  }
+const callYelp = (taskString, user) => {
+  const queryString = taskString.split(' ').join('%20');
+
+
+  console.log('QUERY STRING', queryString);
+
+  // const options = {
+  //   headers: {
+  //     accept: 'application/json',
+  //     Authorization: 'jI8tcWkGvSze503qbpZDTTcX03EOu_WQQTM6_szO8eItEuBqCdSr-Y8Mu51KSkjqElalp0EqHcKxRA6Zs_cwUsCvK6Ww0tkr9lANZcGqvOfflxtbPPbrRe0hkUKjY3Yx'
+  //   }
+  // };
+
+  // https://api.yelp.com/v3/businesses/search?latitude=51.04553&longitude=-114.073129&term=Bridgette%20Bar&sort_by=best_match&limit=20
+
+  axios.get(`https://api.yelp.com/v3/businesses/search?location=${user.location}&term=${taskString}&sort_by=best_match&limit=20`,
+    {
+      headers: {
+        Authorization: `BEARER ${process.env.YPAPPKEY}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => console.log('RESPONSE', response))
+    .catch(err => console.error(err));
+
 };
 
-axios.get('https://api.yelp.com/v3/businesses/search?latitude=51.04553&longitude=-114.073129&term=Bridgette%20Bar&sort_by=best_match&limit=20', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+const user = {
+  location: 'vancouver'
+};
 
+callYelp('miku', user);
 
-  51.045530, -114.073129
+module.exports = { callYelp };
--- services/select_category.js
+++ services/select_category.js
@@ -1,1 +1,47 @@
+const selectCategory = (taskString) => {
+  const input = taskString.toLowerCase()
 
+  let category = null;
+
+  if (
+    input.includes('watch') ||
+    input.includes('film') ||
+    input.includes('movie') ||
+    input.includes('tv') ||
+    input.includes('episode')
+  ) {
+    category = 'to_watch'
+  } else if (
+    input.includes('eat') ||
+    input.includes('resturant') ||
+    input.includes('bar') ||
+    input.includes('food') ||
+    input.includes('diner') ||
+    input.includes('dish') ||
+    input.includes('meal') ||
+    input.includes('dinner') ||
+    input.includes('breakfast') ||
+    input.includes('brunch')
+  ) {
+    category = 'to_eat'
+  } else if (
+    input.includes('read') ||
+    input.includes('book') ||
+    input.includes('novel') ||
+    input.includes('article') ||
+    input.includes('report') ||
+    input.includes('textbook')
+  ) {
+    category = 'to_read'
+  } else if (
+    input.includes('buy') ||
+    input.includes('purchase') ||
+    input.includes('shopping') ||
+    input.includes('store') ||
+    input.includes('report') ||
+    input.includes('textbook')
+  ) {
+    category = 'to_buy'
+  }
+  return category
+}
