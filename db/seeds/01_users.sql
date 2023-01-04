33f37d44120b0fb6398188fda087e6565000e593
Thien Trieu <thien.trieu@telus.com>
2023-01-04 14:53:35 -0800

    add more seeds to todo items for user ID 1 for demo

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here
-INSERT INTO users (name, email, password, location) VALUES ('Britney', 'itsbritney@spears.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'montreal');
+INSERT INTO users (name, email, password, location) VALUES ('Britney', 'itsbritney@spears.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'los angeles');
 INSERT INTO users (name, email, password, location) VALUES ('Beyonce', 'kirauser2@any.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'edmonton');
 INSERT INTO users (name, email, password, location) VALUES ('Justin', 'user3@timberlake.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'calgary');
 INSERT INTO users (name, email, password, location) VALUES ('Slimshady', 'marshal@mathers.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'toronto');
