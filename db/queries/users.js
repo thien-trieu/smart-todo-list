c38f8101d0184f2ad1ae8a0562464d2513d1bcbe
Thien Trieu <thien.trieu@telus.com>
2023-01-04 12:15:28 -0800

    Remove unused files from APP boiler template. getUsers() function. users-api.js route. and links for route in server.js file

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,12 +1,5 @@
 const db = require('../connection');
 
-// const getUsers = () => {
-//   return db.query('SELECT * FROM users;')
-//     .then(data => {
-//       return data.rows;
-//     });
-// };
-
 const getUserByEmail = (email) => {
   return db.query('SELECT * FROM users WHERE email = $1;', [email])
     .then(data => {
